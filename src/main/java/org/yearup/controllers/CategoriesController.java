3ed497cd83df47c20098f4fe09e7762f9dba6768
anikiti3 <anikiti3@email.cpcc.edu>
2024-01-02 10:53:49 -0500

    Added get all categories

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,19 +1,21 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -21,12 +23,20 @@ public class CategoriesController
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
+    @Autowired
+public CategoriesController(CategoryDao categoryDao, ProductDao productDao){
+    this.categoryDao = categoryDao;
+    this.productDao = productDao;
+}
     // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
+    @RequestMapping(path="/http://localhost:8080/categories",method = RequestMethod.GET)
+    @PreAuthorize("permitAll()")
+    public List<Category> getAll(){
+
+
         // find and return all categories
-        return null;
+
+        return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
