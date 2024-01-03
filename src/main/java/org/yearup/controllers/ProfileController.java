db571afeb6645e6c73506359a44be4cb29038e5d
Randy23123 <rlopez@appdev.yearup.org>
2024-01-03 10:36:37 -0500

    added put method for profile

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -3,6 +3,7 @@ package org.yearup.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
@@ -34,6 +35,21 @@ public class ProfileController {
         } catch (Exception e) {
             e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.", e);
+        }
+    }
+
+    @RequestMapping(path = "/profile/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> updateProfile(@PathVariable int userId, @RequestBody Profile profile) {
+        try {
+            Profile updatedProfile = profileDao.update(userId, profile);
+            if (updatedProfile != null) {
+                return new ResponseEntity<>("Profile updated successfully", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Profile not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error updating profile: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
